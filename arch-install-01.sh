2f9927a6bd3e5c320c579f4065102b5d2a91ba3d
user <user@pop-os.localdomain>
2020-10-06 23:34:28 +0100

    maybe fix

--- arch-install-01.sh
+++ arch-install-01.sh
@@ -3,7 +3,7 @@
 . ./arch-config.sh
 
 partition_bios_encrypted(){
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/"$drive"
     o # clear the in memory partition table
     n # new partition
     p # primary partition
