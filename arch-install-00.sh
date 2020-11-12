6ea530aa8bb2b16acbaa87c6dcf0a80785d20645
james <james_d02@protonmail.com>
2020-11-12 12:03:11 +0000

    password fix

--- arch-install-00.sh
+++ arch-install-00.sh
@@ -93,7 +93,7 @@ get_user_input(){
 
     read -r -s -p "Enter root password: " pass1; echo ''
     read -r -s -p "Re-enter root password: " pass2; echo ''
-    while [ "$pass1" != "$pass2" ]; do
+    while [[ "$pass1" != "$pass2" || -z "$pass1" ]]; do
         echo "Passwords do not match, please retry."
         read -r -s -p "Enter root password: " pass1; echo ''
         read -r -s -p "Re-enter root password: " pass1; echo ''
@@ -102,7 +102,7 @@ get_user_input(){
 
     read -r -s -p "Enter user password: " pass1; echo ''
     read -r -s -p "Re-enter user password: " pass2; echo ''
-    while [ "$pass1" != "$pass2" ]; do
+    while [[ "$pass1" != "$pass2" || -z "$pass1" ]]; do
         echo "Passwords do not match, please retry."
         read -r -s -p "Enter user password: " pass1; echo ''
         read -r -s -p "Re-enter user password: " pass2; echo ''
