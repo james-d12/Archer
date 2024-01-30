f170dd1242efcebafd573e9347a51d0feb1a109c
James Durban <james_d02@protonmail.com>
2024-01-30 21:48:24 +0000

    echoed de packages

--- scripts/arch-install-02.sh
+++ scripts/arch-install-02.sh
@@ -119,6 +119,7 @@ function install_package(){
 }
 
 function install_pacman_packages(){
+    echo -ne "DE PACKAGES: ${depackages[@]}"
     sudo pacman -S --noconfirm --needed "${depackages[@]}" || error "Could not install desktop environment packages, as one of the packages is invalid."
     sudo pacman -S --noconfirm --needed "${pacman_packages[@]}" || error "Could not install pacman packages, as one of the packages is invalid."
 }
