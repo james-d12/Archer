d3f336187fee60938a01de40009c40262d462c81
James Durban <james_d02@protonmail.com>
2024-01-30 22:09:21 +0000

    changes

--- scripts/arch-install-02.sh
+++ scripts/arch-install-02.sh
@@ -175,7 +175,7 @@ function install_packages(){
 
 function install_all_packages(){
     while IFS=, read -r installer package description; do
-        add_package_to_list "$installer" "$package" 
+        add_package_to_list "$installer" $package
     done < "/home/$ARCHER_USER/arch-install-scripts/scripts/resources/programs.csv"; 
     install_packages
 }
