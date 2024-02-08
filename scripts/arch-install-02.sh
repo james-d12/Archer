c2323829f47df70b2a462c9836ac8364f423cb12
James Durban <james_d02@protonmail.com>
2024-02-08 23:38:43 +0000

    taken from reddit post lol

--- scripts/arch-install-02.sh
+++ scripts/arch-install-02.sh
@@ -90,7 +90,11 @@ function check_network_connection(){
 }
 
 function check_repositories() {
-    sed -i 's/#[multilib]/[multilib]/' filename
+    cp /etc/pacman.conf /etc/pacman.conf.backup
+    mline=$(grep -n "\\[multilib\\]" /etc/pacman.conf | cut -d: -f1)
+    rline=$(($mline + 1))
+    sed -i ''$mline's|#\[multilib\]|\[multilib\]|g' /etc/pacman.conf
+    sed -i ''$rline's|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g' /etc/pacman.conf
 }
 
 pacman_packages=()
